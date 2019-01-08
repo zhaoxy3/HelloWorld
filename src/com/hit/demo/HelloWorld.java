247f5e4ee7d48b749a379f6bbac254159fb2e78d
zhaoxuyuan <zhaoxuyuan@bonc.com.cn>
2019-01-08 15:19:30 +0800

    Initial commit

---
+++ src/com/hit/demo/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.hit.demo;
+
+/**
+ * Author:zhaoxy
+ * Date:2019/1/8 10:56
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Word");
+
+    }
+
+}
---
+++ src/com/hit/demo/test.java
@@ -0,0 +1,12 @@
+package com.hit.demo;
+
+import java.io.Serializable;
+
+/**
+ * Author:zhaoxy
+ * Date:2019/1/8 11:51
+ */
+public class test implements Serializable {
+
+    private static final long serialVersionUID = 614215410888268423L;
+}
